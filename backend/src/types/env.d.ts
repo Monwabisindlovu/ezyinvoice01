5844f0cd765cf8e4a6888043ed1fb37f10d4951a
monwabisindlovu <monwabisindlovu78@gmail.com>
2025-03-14 10:26:21 +0200

    url update

--- backend/src/types/env.d.ts
+++
@@ -1,8 +0,0 @@
-declare global {
-  namespace NodeJS {
-    interface ProcessEnv {
-      GOOGLE_CLIENT_ID: string; // Required string for Google Client ID
-      JWT_SECRET: string; // Required string for JWT Secret
-    }
-  }
-}
